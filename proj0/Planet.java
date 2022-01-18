78a5d32eb0cfeba2edd516615fb9a4f4299e65f0
MIKETAT <496466932@qq.com>
2022-01-19 00:02:33 +0800

    sixth submit proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,7 +6,7 @@ public class Planet {
 	public double yyVel; /** Its current velocity in the y direction */
 	public double mass;	 /** Its mass */
 	public String imgFileName;
-	public static double G = 6.67e-11;	/** Ggravitational constant */
+	private static double G = 6.67e-11;	/** gravitational constant */
 
 
 	public Planet(double xP, double yP, double xV,
