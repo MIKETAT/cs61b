f4b3cb4500764b9bd31d8e9868d18390cb73fc90
MIKETAT <496466932@qq.com>
2022-01-22 11:46:14 +0800

    second commit lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null) {
+            return B;
+        }
+        if(B == null) {
+            return A;
+        }
         IntList p = A;
         while(p.rest != null) {
             p = p.rest;
